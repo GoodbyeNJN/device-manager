1b257d339e60c4e97c48aa8caf5276d4e22ba205
GoodbyeNJN <cc@fuckwall.cc>
2022-06-28 18:51:31 +0800

    add global interceptor

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -2,11 +2,13 @@ import morgan from 'morgan';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@app/config';
 import { AppModule } from './modules/app';
+import { TransformInterceptor } from './interceptors/transform';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
 
   app.use(morgan('tiny'));
+  app.useGlobalInterceptors(new TransformInterceptor());
   app.setGlobalPrefix('api');
 
   const configService = app.get(ConfigService);
