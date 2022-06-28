c45f1aee55956a57e2882e0693860e55d3fb12d0
GoodbyeNJN <cc@fuckwall.cc>
2022-06-28 18:50:57 +0800

    add log for each request

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -1,17 +1,16 @@
+import morgan from 'morgan';
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
 import { ConfigService } from '@app/config';
 import { AppModule } from './modules/app';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
 
+  app.use(morgan('tiny'));
   app.setGlobalPrefix('api');
 
   const configService = app.get(ConfigService);
   const { host, port } = configService.mainConfig.listen;
-
   await app.listen(port, host);
-  Logger.log(`[apps/main] Listening on ${await app.getUrl()}`);
 };
 bootstrap();
