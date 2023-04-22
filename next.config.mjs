db0dcb5e28893c67fd3b9b9978e0876aef656d85
GoodbyeNJN <cc@fuckwall.cc>
2023-04-23 03:52:48 +0800

    ci: fix env validation error

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 import AutoImport from "unplugin-auto-import/webpack";
 import { $, log } from "zx";
 
-$.log = entry => entry.kind !== "cmd" && log(entry);
-await $`npx tsx ./env`;
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+if (!process.env.SKIP_ENV_VALIDATION) {
+    $.log = entry => entry.kind !== "cmd" && log(entry);
+    await $`npx tsx ./env`;
+}
 
 /** @type {import("next").NextConfig} */
 const config = {
